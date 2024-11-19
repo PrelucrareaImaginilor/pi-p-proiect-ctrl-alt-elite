99e1d0ef53c481e9b7d818c585e11b89e580b991
Munteanu Radu-Stefan <radu.stefan2030@gmail.com>
2024-11-19 20:54:31 +0200

    Implementarea preprocesarii setului de date

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import torch.nn as nn
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
+#preprocseasare set de date
+IMG_SIZE = 128
+BATCH_SIZE = 32
+transform = transforms.Compose([
+    transforms.Resize((IMG_SIZE, IMG_SIZE)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
+
+train_dataset = datasets.ImageFolder(root='DATASET/test', transform=transform)
+val_dataset = datasets.ImageFolder(root='DATASET/train', transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+#arhitectura cnn
 class CNNModel(nn.Module):
     def __init__(self):
         super(CNNModel, self).__init__()
